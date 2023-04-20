84c76369caa69d2cef7f28067598c29c1629bf99
paul-tes <paulasca8462@gmail.com>
2023-04-20 19:30:22 +0300

    add menue popup

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// js code
+// menue button event
+const menueBtn = document.querySelector('.menue-button');
+const menueClose = document.querySelector('.close-mob-menue');
+// open menue
+menueBtn.addEventListener('click', () => {
+  const menuContainer = document.querySelector('.menue-container');
+  menuContainer.classList.add('display-block');
+  menuContainer.style.width = '95%';
+})
+// close menue
+menueClose.addEventListener('click', () => {
+  const menuContainer = document.querySelector('.menue-container');
+  menuContainer.classList.remove('display-block');
+})
+
+
+const instructors = [
+  {
+    name: 'Daniel James',
+    photo: '',
+    title: 'software desinger and project manager at google',
+    backgroung: 'James studies software enginering in harvard university lasvegas, publishes 2n2 application'
+  },
+
+]
